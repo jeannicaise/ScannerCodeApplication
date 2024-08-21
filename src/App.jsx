828278648733abb0393c263c31be14c53cde4a71
jeannicaise <jeannicaise2468@gmail.com>
2024-08-21 18:17:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import QRScanner from './QRscanner' ;
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Guest List Scanner</h1>
+      </header>
+      <QRScanner />
+    </div>
+   
+  );
+}
+
+export default App
